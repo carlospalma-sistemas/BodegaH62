f7c3b29cad24cc76941cddeebfa36d1da0d5fa7a
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2022-07-19 20:17:53 -0500

    Limpiar la bodega y quitar archivo

--- Codigo/Bodega.java
+++ Codigo/Bodega.java
@@ -7,95 +7,53 @@ public class Bodega
     
     public Bodega()
     {
-        ArchivoProductos archivo = new ArchivoProductos();
-        this.listaProductos = archivo.leerProductos();
+        this.listaProductos = new ArrayList<Producto>();
     }
     
     public List<Producto> getListaProductos()
     {
-        ArchivoProductos archivo = new ArchivoProductos();
-        this.listaProductos = archivo.leerProductos();
         return this.listaProductos;
     }
 
     public void agregarProducto(Producto p) 
     {
-        this.listaProductos.add(p);
-        ArchivoProductos a = new ArchivoProductos();
-        a.guardarProducto(p.toCSV());
+        
     }
 
     public int buscarProducto(String codigo) 
     {
-        for (int i = 0; i < this.listaProductos.size(); i++)
-        {
-            if (this.listaProductos.get(i).getCodigo().equals(codigo))
-            {
-                return i;
-            }
-        }
         return -1;
     }
     
     public ArrayList<Producto> buscarProductos(String criterio)
     {
         ArrayList<Producto> listaEncontrados = new ArrayList<Producto>();
-        for (Producto p : this.listaProductos)
-        {
-            if (p.getNombre().equals(criterio) || p.getMarca().equals(criterio) || p.getPresentacion().equals(criterio) || p.getTipo().equals(criterio))
-            {
-                listaEncontrados.add(p);
-            }
-        }
         return listaEncontrados;
     }
     
     public void eliminarProducto(String codigo) 
     {
-        int indexAEliminar = this.buscarProducto(codigo);
-        if (indexAEliminar != -1)
-        {
-            this.listaProductos.remove(indexAEliminar);    
-        }
+        
     }
 
    
     public void modificarPrecio(String codigo, int precio) 
     {
-        int indexAModificar = this.buscarProducto(codigo);
-        if (indexAModificar != -1)
-        {
-            this.listaProductos.get(indexAModificar).setPrecio(precio);
-        }
+        
     }
 
     public void incrementarCantProducto(String codigo, int cant) 
     {
-        int indexAModificar = this.buscarProducto(codigo);
-        if (indexAModificar != -1)
-        {
-            int cantActual = this.listaProductos.get(indexAModificar).getCantidad();
-            this.listaProductos.get(indexAModificar).setCantidad(cantActual + cant);
-        }
+
     }
 
     public void disminuirCantProducto(String codigo, int cant) 
     {
-        int indexAModificar = this.buscarProducto(codigo);
-        if (indexAModificar != -1)
-        {
-            int cantActual = this.listaProductos.get(indexAModificar).getCantidad();
-            if (cantActual >= cant)
-            {
-                this.listaProductos.get(indexAModificar).setCantidad(cantActual - cant);    
-            }
-        }
+
     }
     
     public void actualizarArchivo()
     {
-        ArchivoProductos archivo = new ArchivoProductos();
-        archivo.guardarLista(this.listaProductos);
     }
 }
 
